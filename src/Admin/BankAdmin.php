7b228d81df7a5d4bc3308860f43d0237c5b89b19
elliot sawyer <354793+elliot-sawyer@users.noreply.github.com>
2018-11-08 23:44:12 +1300

    add a modeladmin

--- src/Admin/BankAdmin.php
+++ src/Admin/BankAdmin.php
@@ -1,2 +1,16 @@
 <?php
-namespace CryptoPay/NZBanks;
+namespace CryptoPay\NZBanks;
+
+use SilverStripe\Admin\ModelAdmin;
+
+
+class BankAdmin extends ModelAdmin
+{
+    private static $managed_models = [
+        'Bank'
+    ];
+
+    private static $menu_title = 'Banks';
+
+    private static $url_segment = 'banks';
+}
